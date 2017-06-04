1e1ea794423429ef0290c378bbe09ab4119b0e5c
sospedra <sospedra.r@gmail.com>
2017-06-04 22:12:21 +0200

    test: Use Ava test runned for sumN

--- core/sumN/index.spec.js
+++ core/sumN/index.spec.js
@@ -1,11 +1,14 @@
+const test = require('ava')
+
 const sumN = require('./index')
 
 const randomInt = () => Number.parseInt(Math.random() * 50)
 
-test('given some numbers should return the sum', () => {
+test('given some numbers should return the sum', (t) => {
   const params = Array(randomInt()).join(0).split(0).map(randomInt)
   const expected = params.reduce((memo, param) => memo + param)
   const result = sumN(...params)
 
-  expect(result).toBe(expected)
+  t.plan(1)
+  t.is(result, expected)
 })
